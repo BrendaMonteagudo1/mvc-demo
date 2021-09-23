924423ac9d382ac60ffca00a6f81ab3957ef515b
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-09-23 14:25:07 -0300

    se agrega cambios en html

--- src/main/java/ar/com/ada/mvc/demo/controllers/HoraController.java
+++ src/main/java/ar/com/ada/mvc/demo/controllers/HoraController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ar.com.ada.mvc.demo.models.UserInfo;
+
 
 @Controller // a diferencia de Rest API, aca son controller y no Restcontroller
 @RequestMapping("/hora") // coloca el requestmapping a nivel general
@@ -16,6 +18,11 @@ public class HoraController {
    @GetMapping("/ver-hora-actual") 
    //el metodo es String por que devuelve la vista a procesar
    public String getHoraActual(Model model){
+     UserInfo userInfo = new UserInfo();
+     userInfo.nombre ="Victoria";
+     userInfo.apellido = "Penaloza";
+     
+     model.addAttribute("usuario", userInfo);
      model.addAttribute("hora", "La hora actual en este momento es: "+ (new Date()).toString());
        return "hora-actual"; // esta es la vista(buscara en la carpeta templates hora-actual)
    }
---
+++ src/main/java/ar/com/ada/mvc/demo/models/UserInfo.java
@@ -0,0 +1,7 @@
+package ar.com.ada.mvc.demo.models;
+
+public class UserInfo {
+    public String nombre;
+    public String apellido;
+    
+}
